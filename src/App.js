304391b7ea5da9b07a2035f87b91a7425d7a54ab
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-11-10 15:52:24 +0500

    Created the initial stack navigator

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { ThemeProvider } from 'styled-components';
 import { Home, CurrencyList, Options, Themes } from './screens';
 import { theme } from './theme';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <ThemeProvider theme={theme}>
-        <SafeAreaProvider></SafeAreaProvider>
+        <SafeAreaProvider>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={Home} />
+            <Stack.Screen name='CurrencyList' component={CurrencyList} />
+            <Stack.Screen name='Options' component={Options} />
+            <Stack.Screen name='Themes' component={Themes} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
       </ThemeProvider>
     </NavigationContainer>
   );
--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -6,7 +6,6 @@ export const StyledContainer = styled.View`
   align-items: ${({ centered }) => (centered ? 'center' : 'flex-start')}
   background-color: ${({ theme, themeBackground }) =>
     themeBackground ? theme.color.blue : theme.color.white}
-  padding-top: ${({ insets }) => `${Math.max(insets.top, 16)}px`}
   padding-bottom: ${({ insets }) => `${Math.max(insets.bottom, 16)}px`}
   padding-left: ${({ insets }) => `${Math.max(insets.left, 16)}px`}
   padding-right: ${({ insets }) => `${Math.max(insets.right, 16)}px`}
