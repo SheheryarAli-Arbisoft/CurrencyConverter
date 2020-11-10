5b02c74d859be060a901d90ef644b4b85f68b75c
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-11-10 15:44:12 +0500

    Added react navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components';
 import { Home, CurrencyList, Options, Themes } from './screens';
 import { theme } from './theme';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaProvider></SafeAreaProvider>
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider></SafeAreaProvider>
+      </ThemeProvider>
+    </NavigationContainer>
   );
 };
 
