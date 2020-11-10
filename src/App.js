cddc07d6537c1d78646fe22b11aa09b76262d7e9
ssssyed98 <ssssyed98@gmail.com>
2020-11-10 12:23:14 +0500

    Added safe area provider in main app file to render components correctly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components';
 import { Home, CurrencyList, Options, Themes } from './screens';
 import { theme } from './theme';
 
 const App = () => {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider></SafeAreaProvider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
