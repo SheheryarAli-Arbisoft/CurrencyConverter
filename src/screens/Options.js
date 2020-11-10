bb99db704f85926fd0f82abb3fc019a576d9c1c3
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-11-10 16:34:31 +0500

    Setup navigation in the options screen

--- src/screens/Options.js
+++ src/screens/Options.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { Linking, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Container } from '../components/Container';
 import { ListItem, ListItemSeparator } from '../components/List';
 
 export const Options = () => {
+  const navigation = useNavigation();
+
+  const URL = 'https://fixer.io';
+
+  const openUrl = () => {
+    Linking.openURL(URL).catch(() =>
+      Alert.alert('Error occurred', 'Please try again later')
+    );
+  };
+
   return (
     <Container>
-      <ListItem text='Themes' icon='chevron-right' />
+      <ListItem
+        text='Themes'
+        icon='chevron-right'
+        onPress={() => navigation.navigate('Themes')}
+      />
       <ListItemSeparator />
 
-      <ListItem text='Fixer.io' icon='google-circles-extended' />
+      <ListItem
+        text='Fixer.io'
+        icon='google-circles-extended'
+        onPress={openUrl}
+      />
       <ListItemSeparator />
 
       <ListItem text='Logout' icon='logout' />
