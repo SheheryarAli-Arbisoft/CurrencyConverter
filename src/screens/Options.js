bb09849171d8b0bd335b85459ee95f2b1f9cc6ca
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-11-11 13:07:58 +0500

    Updated .env file and integrated redux in the application

--- src/screens/Options.js
+++ src/screens/Options.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Linking, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { WEBSITE_URL } from '@env';
 import { Container } from '../components/Container';
 import { StatusBar } from '../components/StatusBar';
 import { ListItem, ListItemSeparator } from '../components/List';
@@ -8,10 +9,8 @@ import { ListItem, ListItemSeparator } from '../components/List';
 export const Options = () => {
   const navigation = useNavigation();
 
-  const URL = 'https://fixer.io';
-
   const openUrl = () => {
-    Linking.openURL(URL).catch(() =>
+    Linking.openURL(WEBSITE_URL).catch(() =>
       Alert.alert('Error occurred', 'Please try again later')
     );
   };
